7bf43c84d86620b9d609a1fb83fceea47e8bdb7e
Brian McFee <brian.mcfee@nyu.edu>
2015-09-29 14:28:22 -0400

    checking in flask scaffolding

---
+++ app/server.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import argparse
+import flask
+import ConfigParser
+import os
+import re
+import sys
+import ujson as json
+
+DEBUG = True
+
+# construct application object
+app = flask.Flask(__name__)
+app.config.from_object(__name__)
+
+def load_config(server_ini):
+    P = ConfigParser.RawConfigParser()
+
+    P.opionxform = str
+    P.read(server_ini)
+
+    CFG = {}
+    for section in P.sections():
+        CFG[section] = dict(P.items(section))
+
+    for (k, v) in CFG['server'].iteritems():
+        app.config[k] = v
+    return CFG
+
+
+def run(**kwargs):
+    app.run(**kwargs)
+
+
+@app.route('/')
+def index():
+    '''Top-level web page'''
+    return flask.render_template('index.html')
+
+
+# Main block
+def process_arguments(args):
+
+    parser = argparse.ArgumentParser(description='entrofy web server')
+
+    parser.add_argument('-i',
+                        '--ini',
+                        dest='ini',
+                        required=False,
+                        type=str,
+                        default='server.ini',
+                        help='Path to server.ini file')
+
+    parser.add_argument('-p',
+                        '--port',
+                        dest='port',
+                        required=False,
+                        type=int,
+                        default=5000,
+                        help='Port')
+
+    parser.add_argument('--host',
+                        dest='host',
+                        required=False,
+                        type=str,
+                        default='0.0.0.0',
+                        help='host')
+
+    return vars(parser.parse_args(args))
+
+
+if __name__ == '__main__':
+    parameters = process_arguments(sys.argv[1:])
+
+    CFG = load_config(parameters['ini'])
+
+    port = parameters['port']
+
+    if os.environ.get('ENV') == 'production':
+        port = int(os.environ.get('PORT'))
+
+    run(host=parameters['host'], port=port, debug=DEBUG, processes=3)
