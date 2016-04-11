83797ee7fd1ad3122b4fc4044c1a3c1bc8724750
Brian McFee <brian.mcfee@nyu.edu>
2016-04-11 14:55:36 -0400

    docstrings for mappers

--- entrofy/mappers.py
+++ entrofy/mappers.py
@@ -23,11 +23,24 @@ def equal_maker(value):
 
 
 class BaseMapper(object):
-
+    '''A generic base class for mapper objects'''
     def __init__(self):
         pass
 
     def transform(self, column):
+        '''Binarize a column
+
+        Parameters
+        ----------
+        column : pd.Series
+            The series to binarize
+
+        Returns
+        -------
+        frame : pd.DataFrame, dtype=float
+            A DataFrame with the same index as `column`, and one binary-valued
+            column for each potential output.
+        '''
         df = pd.DataFrame(index=column.index,
                           columns=sorted(list(self.targets.keys())),
                           dtype=float)
@@ -42,14 +55,40 @@ class BaseMapper(object):
 
 
 class ObjectMapper(BaseMapper):
+    '''A generic object-mapper.
 
+    This mapper is appropriate for strings or categorical types.
+
+    Attributes
+    ----------
+    targets : dict
+        A dictionary mapping output column names to target probabilities
+    '''
     def __init__(self, column, prefix='', n_out=None, targets=None):
+        '''Object mapper.
 
-        # 1. determine unique values
+        Parameters
+        ----------
+        column : pd.Series
+            A pandas Series object
+
+        prefix : str
+            A string to prepend to output column names
+
+        n_out : int or None
+            maximum number of output columns to generate.
+            If fewer than the number of unique values in `column`, the
+            most populous values are selected first.
+
+        targets: dict {value: probability}
+            An optional pre-computed target dictionary.
+            If provided, then `n_out` and `prefix` are ignored.
+        '''
         if targets is not None:
             self.targets = targets
             self._map = {v: equal_maker(v) for v in targets}
         else:
+            # 1. determine unique values
             values = column.value_counts().index
 
             if n_out is None:
