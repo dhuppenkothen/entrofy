eac8767ed5105896210e4b1a458a69c47d27cd20
Jacob Deppen <deppen.8@gmail.com>
2019-05-16 09:40:35 -0700

    apply Black formatting

--- entrofy/utils.py
+++ entrofy/utils.py
@@ -5,6 +5,7 @@
 import numbers
 import numpy as np
 
+
 # The following is borrowed from scikit-learn v0.17
 def check_random_state(seed):
     """Turn seed into a np.random.RandomState instance
@@ -20,5 +21,6 @@ def check_random_state(seed):
     if isinstance(seed, np.random.RandomState):
         return seed
     raise ValueError(
-        "%r cannot be used to seed a numpy.random.RandomState" " instance" % seed
+        "%r cannot be used to seed a numpy.random.RandomState"
+        " instance" % seed
     )
