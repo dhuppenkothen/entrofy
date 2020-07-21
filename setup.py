2e46855d1092c6db72e2ac3c39bed0be203fba5e
Jacob Deppen <deppen.8@gmail.com>
2020-07-20 22:02:04 -0700

    add Python 3.6, 3.7, 3.8 to setup.py metadata

--- setup.py
+++ setup.py
@@ -24,6 +24,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     license='ISC',
     install_requires=[
